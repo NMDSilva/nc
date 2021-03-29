d1317b7c77732d7f02b80e6368a9a66d9189344d
NMDSilva <silvanuno@gmail.com>
2021-03-29 21:11:50 +0000

    Alterações estruturais

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 window.$ = window.jQuery = require('jquery');
 
 // Configurações
-const openMenu = document.querySelector('.icon-menu');
 const menu = document.querySelector('nav');
 const carregando = document.querySelector('.carregando');
 const conteudo = document.querySelector('#conteudoPagina');
@@ -10,30 +9,20 @@ const menuClientes = document.querySelector('#menuClientes');
 const menuServicos = document.querySelector('#menuServicos');
 const menuConfigs = document.querySelector('#menuConfigs');
 const menuSobre = document.querySelector('#menuSobre');
+
 // Funções
-const toggleMenuHandler = () => {
-  openMenu.classList.toggle('ativo');
-  menu.classList.toggle('ativo');
-};
 const abrirMenu = (pagina) => {
   carregando.classList.add('mostrar');
   $.get(
     `paginas/${pagina}.html`,
     res => {
-      setTimeout(
-        () => {
-          conteudo.innerHTML = res;
-          carregando.classList.remove('mostrar')
-
-        },
-        10000
-      );
+      conteudo.innerHTML = res;
+      carregando.classList.remove('mostrar')
     }
   )
 };
 
 // Ações
-openMenu.addEventListener('click', toggleMenuHandler);
 menuHome.addEventListener('click', abrirMenu.bind(this, 'home', 'Home'));
 menuClientes.addEventListener('click', abrirMenu.bind(this, 'clientes', 'Clientes'));
 menuServicos.addEventListener('click', abrirMenu.bind(this, 'servicos', 'Serviços'));
