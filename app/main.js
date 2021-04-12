465c829334560a31f35776a154fdd6e4b2130014
NMDSilva <silvanuno@gmail.com>
2021-04-12 22:08:46 +0000

    alterações login instalação moment

--- app/main.js
+++ app/main.js
@@ -1,21 +1,50 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const moment = require('moment');
+const Store = require('./store');
 const { sequelize, User } = require('../data/models');
 
-sequelize.authenticate()
-.then(() => { console.log('conection successful'); })
-.catch(err => { console.log(`Error: ${err}`); });
+const store = new Store({
+  configName: 'user-config',
+  defaults: {
+    windowSize: { width: 1024, height: 800 },
+    userInfo: {
+      login: false,
+      user: null,
+      name: null
+    }
+  }
+});
 
-function createWindow () {
+sequelize.authenticate()
+  .then(() => { console.log('conection successful'); })
+  .catch(err => { console.log(`Error: ${err}`); });
+
+async function createWindow() {
+  let { width, height } = store.get('windowSize');
+  const userInfo = store.get('userInfo');
   const win = new BrowserWindow({
-    width: 1024,
-    height: 800,
+    width: width,
+    height: height,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      // contextIsolation: true
     }
   });
 
+  win.on('resize', () => {
+    let { width, height } = win.getBounds();
+    store.set('windowSize', { width, height });
+  });
+  if (userInfo.remeber) {
+    const user = await User.findOne({ where: { uuid: userInfo.uuid } });
+    if (user && moment().format() <= userInfo.validate) {
+      console.log('entrou');
+      win.loadFile(path.join(__dirname, '..', 'src', 'index.html'));
+      return;
+    }
+  }
   win.loadFile(path.join(__dirname, '..', 'src', 'login.html'));
 }
 
@@ -35,20 +64,22 @@ app.on('activate', () => {
 })
 
 ipcMain.on('user:login', async (event, userInfo) => {
-  
   const user = await User.findOne({ where: { email: userInfo.user } });
-
-  console.log(user.password);
-  
-  console.log(bcrypt.hashSync(userInfo.pass, 10), userInfo.pass);
-
-
-  if (user?.password === bcrypt.hashSync(userInfo.pass, 10)) {
-    event.returnValue = {
+  if (bcrypt.compareSync(userInfo.pass, user.password)) {
+    const userData = {
+      ...user.dataValues,
+      id: undefined,
+      password: undefined,
       login: true,
-      user: userInfo.user,
-      nome: user.name
-    };
+      remeber: userInfo.remeber,
+      // validate: moment().add(7, 'd').format()
+      validate: moment().add(1, 'm').format()
+    }
+    store.set('userInfo', userData);
+    event.returnValue = userData;
   }
-  else { event.returnValue = { login: false }; }
+  else {
+    store.set('userInfo', { login: false });
+    event.returnValue = { login: false };
+  }
 });
--- src/js/login.js
+++ src/js/login.js
@@ -5,8 +5,7 @@ const userInput = $('#user');
 const userParent = userInput.parent('.inputBx');
 const passInput = $('#password');
 const passParent = passInput.parent('.inputBx');
-const loginForm = $('#loginForm');
-const recuperar = $('#recuperar');
+const remeberInput = $('#remeber');
 const emailReg = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
 userInput.on('focus', _ => { userParent.removeClass('erro'); });
@@ -32,8 +31,7 @@ const validarFormulario = () => {
     userParent.find('span').html('Não é um email válido');
     return false;
   }
-  
-  const user = ipcRenderer.sendSync('user:login', { user: userInput.val(), pass: passInput.val() });
+  const user = ipcRenderer.sendSync('user:login', { user: userInput.val(), pass: passInput.val(), remeber: remeberInput.is(':checked') });
   
   if (!user.login) {
     userParent.addClass('erro');
