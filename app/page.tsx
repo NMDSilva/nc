c558f8004dbed35f0bbed9ac7a309f459d2c9b35
Nuno Silva <silvanuno@gmail.com>
2024-01-29 13:27:30 +0000

    menu

--- app/page.tsx
+++ app/page.tsx
@@ -10,27 +10,21 @@ export default function Home() {
           <a className="navbar-brand" href="#">
             <Image src={logo} alt="Logo NC - Desinfestações"></Image>
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-            <div className="offcanvas-header">
-              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div className="offcanvas-body">
-              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Serviços</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">Contactos</a>
-                </li>
-              </ul>
-            </div>
+          <div className="collapse navbar-collapse" id="navbarScroll">
+            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{['--bs-scroll-height' as any]: '100px' }}>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Serviços</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contactos</a>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
