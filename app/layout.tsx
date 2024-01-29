adfbfe514e6ed37f05fb7e26d34e3c0ab0ac6020
Nuno Silva <silvanuno@gmail.com>
2024-01-29 12:22:12 +0000

    style: :art: Criação de menu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.css'
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,6 +9,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "NC - Desinfestações",
   description: "Landing page",
+  authors: [
+    {
+      name: 'Nuno Silva'
+    }
+  ]
 };
 
 export default function RootLayout({
@@ -15,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body className={inter.className}>{children}</body>
     </html>
   );
