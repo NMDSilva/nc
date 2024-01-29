9f896b04be54c07742f211b53739d1f5c73c784d
Nuno Silva <silvanuno@gmail.com>
2024-01-29 14:29:05 +0000

    menu

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,21 @@ import logo from './images/logo.svg'
 export default function Home() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar bg-body-tertiary fixed-top">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
             <Image src={logo} alt="Logo NC - Desinfestações"></Image>
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarScroll">
-            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{['--bs-scroll-height' as any]: '100px' }}>
+          <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-end flex-grow-1">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
@@ -25,6 +30,7 @@ export default function Home() {
                 <a className="nav-link" href="#">Contactos</a>
               </li>
             </ul>
+            </div>
           </div>
         </div>
       </nav>
