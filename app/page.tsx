9aed2403762239dd8f13f1510596930d288db96a
Nuno Silva <silvanuno@gmail.com>
2024-01-29 12:45:38 +0000

    Configuração menu

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,11 @@ export default function Home() {
           <a className="navbar-brand" href="#">
             <Image src={logo} alt="Logo NC - Desinfestações"></Image>
           </a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Abrir/Fechar menu">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
